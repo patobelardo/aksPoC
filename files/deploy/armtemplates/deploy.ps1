44099c3e4b058acb6988a67af27d491d8b0d8e6c
patobelardo <patobelardo@outlook.com>
2020-01-17 12:21:45 -0500

    Work

--- files/deploy/armtemplates/deploy.ps1
+++ files/deploy/armtemplates/deploy.ps1
@@ -4,6 +4,6 @@ PARAM(
 )
 az group create -n $rgName -l $location
 
-az group deployment create -n aks-deploy -g $rgName --template-file vnet.json 
+az group deployment create -n aks-deploy -g $rgName --template-file vnet.json  --parameters '@vnet.parameters.json'
 
-az group deployment create -n aks-deploy -g $rgName --template-file aks-cluster.json --parameters-file @aks-cluster.parameters.json
+az group deployment create -n aks-deploy-cluster -g $rgName --template-file aks-cluster.json --parameter @aks-cluster.parameters.json
--- files/deploy/armtemplates/deploy.sh
+++ files/deploy/armtemplates/deploy.sh
@@ -5,4 +5,4 @@ az group create -n $RGNAME -l $LOCATION
 
 az group deployment create -n aks-deploy -g $RGNAME --template-file vnet.json 
 
-az group deployment create -n aks-deploy -g $RGNAME --template-file aks-cluster.json --parameters @aks-cluster.parameters.json
+az group deployment create -n aks-deploy-cluster -g $RGNAME --template-file aks-cluster.json --parameters @aks-cluster.parameters.json
