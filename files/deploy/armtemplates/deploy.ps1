6ddfac59ca2a96d61d994857ec222239635671f0
Patricio Belardo <pbelardo@microsoft.com>
2020-01-17 13:07:04 -0500

    parameters fix

--- files/deploy/armtemplates/deploy.ps1
+++ files/deploy/armtemplates/deploy.ps1
@@ -6,4 +6,7 @@ az group create -n $rgName -l $location
 
 az group deployment create -n aks-deploy -g $rgName --template-file vnet.json  --parameters '@vnet.parameters.json'
 
-az group deployment create -n aks-deploy-cluster -g $rgName --template-file aks-cluster.json --parameter @aks-cluster.parameters.json
+(Get-Content ./aks-cluster.parameters.json) -replace "##rgName##", $rgName > aks-cluster.parameters.tempps.json
+az group deployment create -n aks-deploy-cluster -g $rgName --template-file aks-cluster.json --parameter '@aks-cluster.parameters.tempps.json'
+
+Remove-Item aks-cluster.parameters.tempps.json
--- files/deploy/armtemplates/deploy.sh
+++ files/deploy/armtemplates/deploy.sh
@@ -3,6 +3,10 @@ LOCATION=eastus
 
 az group create -n $RGNAME -l $LOCATION
 
-az group deployment create -n aks-deploy -g $RGNAME --template-file vnet.json 
+az group deployment create -n aks-deploy -g $RGNAME --template-file vnet.json --parameters @vnet.parameters.json
 
-az group deployment create -n aks-deploy-cluster -g $RGNAME --template-file aks-cluster.json --parameters @aks-cluster.parameters.json
+sed "s/##rgName##/$RGNAME/g" aks-cluster.parameters.json > aks-cluster.parameters.temp.json
+
+az group deployment create -n aks-deploy-cluster -g $RGNAME --template-file aks-cluster.json --parameters @aks-cluster.parameters.temp.json
+
+rm aks-cluster.parameters.temp.json
